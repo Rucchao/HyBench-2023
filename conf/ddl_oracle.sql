1b8de510eef2b5007fec54ee5103ef0d8b8cfe86
unknown <dps@dameng.com>
2023-03-15 11:10:31 +0800

    support oracle

---
+++ conf/ddl_oracle.sql
@@ -0,0 +1,111 @@
+CREATE
+        TABLE CUSTOMER
+        (
+                custID       int PRIMARY KEY,
+                companyID    int            ,
+                gender       char(6)        ,
+                name         char(15)       ,
+                age          int            ,
+                phone        char(11)       ,
+                province     char(15)       ,
+                city         char(15)       ,
+                loan_balance real           ,
+                s_credit     int            ,
+                c_credit     int            ,
+                b_credit     int            ,
+                Isblocked    int            ,
+                created_date Date           ,
+                last_update_timestamp timestamp
+        );
+CREATE
+        TABLE COMPANY
+        (
+                companyID    int PRIMARY KEY,
+                name         varchar(60)    ,
+                category     varchar(50)    ,
+                staff_size   int            ,
+                loan_balance real           ,
+                phone        char(11)       ,
+                province     char(15)       ,
+                city         char(15)       ,
+                SavingCredit int            ,
+				CheckingCredit int			,
+				LoanCredit	 int			,
+                Isblocked    int            ,
+                created_date Date           ,
+                last_update_timestamp timestamp
+        );
+CREATE
+        TABLE SAVINGACCOUNT
+        (
+                accountID int PRIMARY KEY,
+                userID    int            ,
+                balance   real           ,
+                Isblocked int            ,
+                timestamp timestamp
+        );
+CREATE
+        TABLE CHECKINGACCOUNT
+        (
+                accountID int PRIMARY KEY,
+                userID    int            ,
+                balance   real           ,
+                Isblocked int            ,
+                timestamp timestamp
+        );
+        
+CREATE SEQUENCE TRANSFER_SEQ INCREMENT BY 1 START WITH 1;
+
+CREATE
+        TABLE TRANSFER
+        (
+                id       int PRIMARY KEY,
+                sourceID int            ,
+                targetID int            ,
+                amount   real           ,
+                type     char(10)       ,
+                timestamp timestamp     ,
+                fresh_ts timestamp default null
+        );
+
+CREATE SEQUENCE CHECKING_SEQ INCREMENT BY 1 START WITH 1;        
+        
+CREATE
+        TABLE CHECKING
+        (
+                id       int PRIMARY KEY,
+                sourceID int            ,
+                targetID int            ,
+                amount   real           ,
+                type     char(10)       ,
+                timestamp timestamp
+        );
+        
+CREATE SEQUENCE LOANAPPS_SEQ INCREMENT BY 1 START WITH 1; 
+
+CREATE
+        TABLE LOANAPPS
+        (
+                id          int PRIMARY KEY,
+                applicantID int            ,
+                amount      real           ,
+                duration    int            ,
+                status      char(12)       ,
+                timestamp timestamp
+        );
+        
+CREATE SEQUENCE LOANTRANS_SEQ INCREMENT BY 1 START WITH 1; 
+        
+CREATE
+        TABLE LOANTRANS
+        (
+                id          int PRIMARY KEY ,
+                applicantID int             ,
+                appID       int             ,
+                amount      real            ,
+                status      char(12)        ,
+                timestamp timestamp         ,
+                duration int                ,
+                contract_timestamp timestamp,
+                delinquency int
+        );

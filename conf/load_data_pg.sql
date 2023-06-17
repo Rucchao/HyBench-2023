1cb978687c627b5565252b0684dedaf590db15e6
Chao Zhang <cycchao@mail.tsinghua.edu.cn>
2023-06-17 10:41:12 +0800

    Update load_data_pg.sql

--- conf/load_data_pg.sql
+++ conf/load_data_pg.sql
@@ -1,15 +1,15 @@
-Copy customer from '/Path-to-File/customer.csv' CSV DELIMITER ',' ;
+\copy customer from '/Path-to-File/customer.csv' CSV DELIMITER ',' ;
 
-Copy company from '/Path-to-File/company.csv' CSV DELIMITER ',' ;
+\copy company from '/Path-to-File/company.csv' CSV DELIMITER ',' ;
 
-Copy transfer from '/Path-to-File/transfer.csv' CSV DELIMITER ',' ;
+\copy transfer from '/Path-to-File/transfer.csv' CSV DELIMITER ',' ;
 
-Copy checking from '/Path-to-File/checking.csv' CSV DELIMITER ',' ;
+\copy checking from '/Path-to-File/checking.csv' CSV DELIMITER ',' ;
 
-Copy checkingAccount from '/Path-to-File/checkingAccount.csv' CSV DELIMITER ',' ;
+\copy checkingAccount from '/Path-to-File/checkingAccount.csv' CSV DELIMITER ',' ;
 
-Copy savingAccount from '/Path-to-File/savingAccount.csv' CSV DELIMITER ',' ;
+\copy savingAccount from '/Path-to-File/savingAccount.csv' CSV DELIMITER ',' ;
 
-Copy loanApps from '/Path-to-File/loanApps.csv' CSV DELIMITER ',' ;
+\copy loanApps from '/Path-to-File/loanApps.csv' CSV DELIMITER ',' ;
 
-Copy loanTrans from '/Path-to-File/loanTrans.csv' CSV DELIMITER ',' ;
+\copy loanTrans from '/Path-to-File/loanTrans.csv' CSV DELIMITER ',' ;
